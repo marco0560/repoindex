80ce40a9a0e4fd32dda3c8ebc521a7aa7f7e0d77
Marco Coppola <marco.n.coppola@gmail.com>
2026-04-10 16:18:39 +0200

    style(version): format generated version module

--- src/repoindex/_version.py
+++ src/repoindex/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '1.11.0.post1.dev33'
-__version_tuple__ = version_tuple = (1, 11, 0, 'post1', 'dev33')
+__version__ = version = "1.11.0.post1.dev33"
+__version_tuple__ = version_tuple = (1, 11, 0, "post1", "dev33")
 
-__commit_id__ = commit_id = 'g7a3635a4f'
+__commit_id__ = commit_id = "g7a3635a4f"
